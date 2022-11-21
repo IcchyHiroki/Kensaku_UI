fdcba86b5a0caa087bbd50e7718aef5cea0402f3
IcchyHiroki <icchyWorks@gmail.com>
2022-11-21 16:04:49 +0900

    Initial Commit

---
+++ Kensaku_TestUI/Kensaku_TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kensaku_TestUIApp.swift
+//  Kensaku_TestUI
+//
+//  Created by Hiroki Ichikura on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Kensaku_TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
